b04860f8a7c54d6f012c3d6a1dd9ab98ad0851ab
Rontard Noah <rontard.noah@gmail.com>
2025-06-27 17:19:13 +0200

    ex 00+01

---
+++ 02/ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef _FIXED_
+	#define _FIXED_
+
+#include <iostream>
+#include <string>
+
+class Fixed
+{
+	private:
+		int _value;
+		static const int _fractional = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed& fixed);
+		Fixed(const int i);
+		Fixed(const float f);
+		~Fixed();
+		Fixed& operator=(const Fixed& other);
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		float toFloat() const;
+		int toInt() const;
+};
+
+#endif
