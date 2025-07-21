082ceeae7b6f4407c2fb438186530f4ac7dfa21a
Noah Rontard <rontard.noah@gmail.com>
2025-07-21 15:20:54 +0200

    ex

---
+++ 08/ex00/easyfind.hpp
@@ -0,0 +1,20 @@
+#ifndef _EASYFIND_
+	#define _EASYFIND_
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+template < typename T >
+typename T::iterator easyfind(T container, int search)
+{
+	typename T::iterator it;
+
+	it = std::find(container.begin(), container.end(), search);
+	if (it == container.end())
+		throw std::runtime_error("Value not found");
+	return it;
+		
+}
+
+#endif
---
+++ 08/ex00/main.cpp
@@ -0,0 +1,43 @@
+#include "easyfind.hpp"
+
+#include <vector>
+#include <list>
+
+int main()
+{
+	std::vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	std::list<int> l;
+	l.push_back(10);
+	l.push_back(20);
+	l.push_back(30);
+	l.push_back(40);
+	l.push_back(50);
+
+	try
+	{
+		easyfind(v, 3);
+		easyfind(v, 8);	
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		easyfind(l, 20);
+		easyfind(l, 90);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	
+}
+
