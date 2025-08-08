1aea6c24e09e14c6ff44f5dab39dce817f3affd3
NoahRontard <rontard.noah@gmail.com>
2025-08-08 15:02:59 +0200

    finish

--- 09/ex02/PmergeMe.cpp
+++ 09/ex02/PmergeMe.cpp
@@ -3,26 +3,19 @@
 PmergeMe::PmergeMe()
 {}
 
-PmergeMe::PmergeMe(const PmergeMe& copy)
+PmergeMe::PmergeMe(const PmergeMe&)
 {
-	*this = copy;
 }
 
 PmergeMe::~PmergeMe()
 {}
 
-PmergeMe& PmergeMe::operator=(const PmergeMe& copy)
+PmergeMe& PmergeMe::operator=(const PmergeMe&)
 {
-	if (this != &copy)
-	{
-		this->_input = copy._input;
-		this->_max = copy._max;
-		this->_min = copy._min;
-	}
 	return *this;
 }
 
-void PmergeMe::ParsingEntry(char **argv, int argc)
+void PmergeMe::ParsingEntry(char **argv, int argc, std::vector<int>& input)
 {
 	for (int i = 1; i < argc; i++)
 	{
@@ -32,78 +25,186 @@ void PmergeMe::ParsingEntry(char **argv, int argc)
 		itoa << temp;
 		if (strargv != itoa.str() || temp < 0)
 			throw std::invalid_argument("Error: Bad input.");
-		this->_input.push_back(temp);
+		input.push_back(temp);
 	}
 }
 
-void PmergeMe::MinMaxTab(int first, int second)
+int BinarySearch(std::vector<int> sorted, int value)
+{
+	int low = 0;
+	int high = sorted.size();
+
+	while (low < high)
+	{
+		int mid = low + (high - low) / 2;
+
+		if (sorted[mid] < value)
+			low = mid + 1;
+		else
+			high = mid;
+	}
+	return low;
+}
+
+void PmergeMe::BinaryCompare(std::vector<int>& sorted, int value)
+{
+	int pos = BinarySearch(sorted, value);
+	sorted.insert(sorted.begin() + pos, value);
+}
+
+void PmergeMe::MinMaxTab(std::vector<int>& max, std::vector<int>& min, int first, int second)
 {
 	if (first < second)
+		std::swap(first, second);
+	max.push_back(first);
+	min.push_back(second);
+}
+
+std::vector<int> PmergeMe::Execute(std::vector<int>& input)
+{
+	size_t n = input.size();
+	if (n <= 1)
+		return input;
+
+	std::vector<int> max, min;
+	for (size_t i = 0; i + 1 < n; i += 2)
+		PmergeMe::MinMaxTab(max, min, input[i], input[i + 1]);
+
+	std::vector<int> sorted = PmergeMe::Execute(max);
+
+	for (size_t i = 0; i < min.size(); ++i)
+		PmergeMe::BinaryCompare(sorted, min[i]);
+	if (n % 2 != 0)
+		PmergeMe::BinaryCompare(sorted, input[n - 1]);
+
+	input = sorted;
+	return input;
+}
+
+void PmergeMe::ParsingEntry(char **argv, int argc, std::deque<int>& input)
+{
+	for (int i = 1; i < argc; i++)
 	{
-		_min.push_back(first);
-		_max.push_back(second);
-	}
-	else if (first >= second)
-	{
-		_max.push_back(first);
-		_min.push_back(second);
+		std::string strargv = argv[i];
+		int temp = std::atoi(strargv.c_str());
+		std::stringstream itoa;
+		itoa << temp;
+		if (strargv != itoa.str() || temp < 0)
+			throw std::invalid_argument("Error: Bad input.");
+		input.push_back(temp);
 	}
 }
 
-void PmergeMe::DivideElemenent(std::vector<int>::iterator it)
+int BinarySearch(std::deque<int> sorted, int value)
 {
-	if (_input.size() % 2 != 0 && it + 1 == _input.end())
+	int low = 0;
+	int high = sorted.size();
+
+	while (low < high)
 	{
-		_max.push_back(*it);
-		return ;
+		int mid = low + (high - low) / 2;
+
+		if (sorted[mid] < value)
+			low = mid + 1;
+		else
+			high = mid;
 	}
-	PmergeMe::MinMaxTab(*it, *(it + 1));
-	if (it + 1 == _input.end() || it + 2 == _input.end())
-		return ;
-	DivideElemenent(it + 2);
+	return low;
 }
 
-int PmergeMe::BinaryCompare(std::vector<int>::iterator minit, int start, int end)
+void PmergeMe::BinaryCompare(std::deque<int>& sorted, int value)
 {
-	int size = ((start + end) / 2);
-	std::vector<int>::iterator maxit = _max.begin() + size;
-	if (*minit > *maxit)
-	{
-		if ((maxit + 1 != _max.end() && *minit < *(maxit + 1)))
-			return _max.insert(maxit + 1, *minit), 0;
-		BinaryCompare(minit, size + 1, end);
-	}
-	else if (*minit <= *maxit)
-	{
-		if (*maxit == *minit || maxit == _max.begin() || (maxit - 1 != _max.begin() && *minit >= *(maxit - 1)))
-			return _max.insert(maxit, *minit), 0;
-		BinaryCompare(minit, start, size);
-	}
-	return 0;
+	int pos = BinarySearch(sorted, value);
+	sorted.insert(sorted.begin() + pos, value);
 }
+
+void PmergeMe::MinMaxTab(std::deque<int>& max, std::deque<int>& min, int first, int second)
+{
+	if (first < second)
+		std::swap(first, second);
+	max.push_back(first);
+	min.push_back(second);
+}
+
+std::deque<int> PmergeMe::Execute(std::deque<int>& input)
+{
+	size_t n = input.size();
+	if (n <= 1)
+		return input;
+
+	std::deque<int> max, min;
+	for (size_t i = 0; i + 1 < n; i += 2)
+		PmergeMe::MinMaxTab(max, min, input[i], input[i + 1]);
+
+	std::deque<int> sorted = PmergeMe::Execute(max);
+
+	for (size_t i = 0; i < min.size(); ++i)
+		PmergeMe::BinaryCompare(sorted, min[i]);
+	if (n % 2 != 0)
+		PmergeMe::BinaryCompare(sorted, input[n - 1]);
+
+	input = sorted;
+	return input;
+}
+
 
 void PmergeMe::FordJonhson(char **argv, int argc)
 {
+	std::vector<int> input;
 	try
 	{
-		PmergeMe::ParsingEntry(argv, argc);
+		PmergeMe::ParsingEntry(argv, argc, input);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 		return ;
 	}
-	std::vector<int>::iterator it = _input.begin();
-	PmergeMe::DivideElemenent(it);
-	std::sort(_max.begin(), _max.end());
-	for (std::vector<int>::iterator minit = _min.begin(); minit != _min.end(); minit++)
+
+	std::cout << "Before : ";
+	for (std::vector<int>::iterator it = input.begin(); it != input.end(); it++)
 	{
-		PmergeMe::BinaryCompare(minit, 0, _max.size());
+		std::cout << *it << " ";
 	}
-	for (std::vector<int>::iterator maxx = _max.begin(); maxx != _max.end(); maxx++)
+	std::cout << std::endl;
+	clock_t start_vec = clock();
+
+	PmergeMe::Execute(input);
+
+	clock_t end_vec = clock();
+	double time_elapsed_vec = static_cast<double>(end_vec - start_vec);
+	std::cout << "After : ";
+	for (std::vector<int>::iterator it = input.begin(); it != input.end(); it++)
 	{
-		std::cout << *maxx << std::endl;
+		std::cout << *it << " ";
 	}
+	std::cout << std::endl;
+	std::cout << "Time to process a range of " << input.size()
+              << " elements with std::vector: " << std::fixed << std::setprecision(6)
+              << time_elapsed_vec << "us\n";
+
+
+	
+	std::deque<int> input_deq;
+	try
+	{
+		PmergeMe::ParsingEntry(argv, argc, input_deq);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return ;
+	}
+	
+	clock_t start_deq = clock();
+
+	PmergeMe::Execute(input_deq);
+
+	clock_t end_deq = clock();
+	double time_elapsed_deq = static_cast<double>(end_deq - start_deq);
+	std::cout << "Time to process a range of " << input_deq.size()
+              << " elements with std::deque: " << std::fixed << std::setprecision(5)
+              << time_elapsed_deq << "us\n";
 }
 
 
--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -2,6 +2,7 @@
 	#define _PMERGEME_
 
 #include <vector>
+#include <deque>
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -9,24 +10,29 @@
 #include <exception>
 #include <algorithm>
 #include <cmath>
+#include <iomanip>
 
 class PmergeMe
 {
 	private:
-		std::vector<int> _input;
-		std::vector<int> _max;
-		std::vector<int> _min;
 	public:
 		PmergeMe();
 		PmergeMe(const PmergeMe& copy);
 		~PmergeMe();
 		PmergeMe& operator=(const PmergeMe& copy);
 
+		
 		void FordJonhson(char **argv, int argc);
-		void ParsingEntry(char **argv, int argc);
-		void DivideElemenent(std::vector<int>::iterator it);
-		int BinaryCompare(std::vector<int>::iterator minit, int size, int scale);
-		void MinMaxTab(int first, int second);
+		
+		std::vector<int> Execute(std::vector<int>& input);
+		void ParsingEntry(char **argv, int argc, std::vector<int>& input);
+		void BinaryCompare(std::vector<int>& sorted, int value);
+		void MinMaxTab(std::vector<int>& max, std::vector<int>& min, int first, int second);
+
+		std::deque<int> Execute(std::deque<int>& input);
+		void ParsingEntry(char **argv, int argc, std::deque<int>& input);
+		void BinaryCompare(std::deque<int>& sorted, int value);
+		void MinMaxTab(std::deque<int>& max, std::deque<int>& min, int first, int second);
 };
 
 #endif 
